97ca62ac113680c281d486a9cca0108f8b5b5a72
Harry Fox <harry.fox.ic@icloud.com>
2014-08-26 18:11:51 +0100

    Add Direction for PacketPlayOutSpawnPainting

---
+++ src/main/java/org/pistonmc/data/Direction.java
@@ -0,0 +1,30 @@
+package org.pistonmc.data;
+
+public enum Direction {
+
+    NORTH(2),
+    EAST(3),
+    SOUTH(0),
+    WEST(1);
+
+    private int id;
+
+    Direction(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Direction valueOf(int id) {
+        for(Direction direction : values()) {
+            if(direction.getId() == id) {
+                return direction;
+            }
+        }
+
+        return null;
+    }
+
+}
