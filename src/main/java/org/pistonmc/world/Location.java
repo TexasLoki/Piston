903087bac89ad8daf66741777d7ea0742f5699ed
Harry Fox <harry.fox.ic@icloud.com>
2014-08-29 00:06:10 +0100

    Add Block coordinates to Location

--- src/main/java/org/pistonmc/world/Location.java
+++ src/main/java/org/pistonmc/world/Location.java
@@ -4,17 +4,26 @@ public class Location {
 
     private World world;
     private double x;
+    private int blockX;
     private double y;
+    private int blockY;
     private double z;
+    private int blockZ;
     private float yaw;
     private float pitch;
 
-    public Location(final World world, final double x, final double y, final double z) {
+    public Location(World world, int blockX, int blockY, int blockZ) {
+        this.world = world;
+        this.blockX = blockX;
+        this.blockY = blockY;
+        this.blockZ = blockZ;
+    }
+
+    public Location(World world, double x, double y, double z) {
         this(world, x, y, z, 0, 0);
     }
 
-    public Location(final World world, final double x, final double y, final double z, final float yaw,
-                    final float pitch) {
+    public Location(World world, double x, double y, double z, float yaw, float pitch) {
         this.world = world;
         this.x = x;
         this.y = y;
@@ -31,13 +40,16 @@ public class Location {
         this.world = world;
     }
 
-
     public double getX() {
         return x;
     }
 
     public void setX(double x) {
         this.x = x;
+    }
+
+    public int getBlockX() {
+        return blockX;
     }
 
     public double getY() {
@@ -48,12 +60,20 @@ public class Location {
         this.y = y;
     }
 
+    public int getBlockY() {
+        return blockY;
+    }
+
     public double getZ() {
         return z;
     }
 
     public void setZ(double z) {
         this.z = z;
+    }
+
+    public int getBlockZ() {
+        return blockZ;
     }
 
     public float getYaw() {
