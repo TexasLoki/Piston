35cb603f6562ced09b8e10b905f8cfb40360f375
Harry Fox <harry.fox.ic@icloud.com>
2014-08-28 18:54:28 +0100

    Add root server jar to Piston

--- src/main/java/org/pistonmc/Piston.java
+++ src/main/java/org/pistonmc/Piston.java
@@ -5,12 +5,19 @@ import org.pistonmc.logging.Logger;
 import org.pistonmc.plugin.JavaPluginManager;
 import org.pistonmc.plugin.protocol.ProtocolManager;
 
+import java.io.File;
+
 public class Piston {
 
     private static Server server;
+    private static File jar;
 
     public static Server getServer() {
         return server;
+    }
+
+    public static File getServerJar() {
+        return jar;
     }
 
     public static Logger getLogger() {
