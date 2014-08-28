f51475a582abd4fd2c78325f71d113b3bcc5112a
Harry Fox <harry.fox.ic@icloud.com>
2014-08-28 23:04:15 +0100

    Revert "Add call() method to Event"
    This reverts commit 76e4ceea4d96c38dccf8b4bcd05da2170d29254b.

--- src/main/java/org/pistonmc/event/Event.java
+++ src/main/java/org/pistonmc/event/Event.java
@@ -1,6 +1,5 @@
 package org.pistonmc.event;
 
-import org.pistonmc.Piston;
 import org.pistonmc.util.ClassUtils;
 
 public class Event {
@@ -8,10 +7,6 @@ public class Event {
     @Override
     public String toString() {
         return ClassUtils.build(this, true);
-    }
-
-    public void call() {
-        Piston.getEventManager().call(this);
     }
 
 }
