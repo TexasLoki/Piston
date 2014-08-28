a24106ae376f5bc0930fefed0dbb5dedc60fa5df
jshh <jsh.github@gmail.com>
2014-08-28 23:46:10 +0100

    Begin writing Entity interfaces.

---
+++ src/main/java/org/pistonmc/World.java
@@ -0,0 +1,22 @@
+package org.pistonmc;
+
+import org.pistonmc.entity.Entity;
+import org.pistonmc.entity.LivingEntity;
+import org.pistonmc.entity.Player;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface World {
+
+	public UUID getUniqueId();
+
+	public String getName();
+
+	public List<Entity> getEntities();
+
+	public List<LivingEntity> getLivingEntities();
+
+	public List<Player> getPlayers();
+
+}
--- src/main/java/org/pistonmc/entity/Player.java
+++ src/main/java/org/pistonmc/entity/Player.java
@@ -2,10 +2,18 @@ package org.pistonmc.entity;
 
 import org.pistonmc.commands.CommandSender;
 
+import java.net.InetSocketAddress;
+
 public interface Player extends LivingEntity, CommandSender {
 
     public String getDisplayName();
 
     public void setDisplayName(String name);
+
+	public String getPlayerListName();
+
+	public void setPlayerListName(String name);
+
+	public InetSocketAddress getAddress();
 
 }
