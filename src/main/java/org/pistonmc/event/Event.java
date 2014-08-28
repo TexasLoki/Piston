76e4ceea4d96c38dccf8b4bcd05da2170d29254b
Harry Fox <harry.fox.ic@icloud.com>
2014-08-28 23:03:51 +0100

    Add call() method to Event

--- src/main/java/org/pistonmc/event/Event.java
+++ src/main/java/org/pistonmc/event/Event.java
@@ -1,5 +1,6 @@
 package org.pistonmc.event;
 
+import org.pistonmc.Piston;
 import org.pistonmc.util.ClassUtils;
 
 public class Event {
@@ -7,6 +8,10 @@ public class Event {
     @Override
     public String toString() {
         return ClassUtils.build(this, true);
+    }
+
+    public void call() {
+        Piston.getEventManager().call(this);
     }
 
 }
