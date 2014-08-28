3e1a01c71834c48c3b1fc3a1f1380b0cb970f87e
Harry Fox <harry.fox.ic@icloud.com>
2014-08-28 18:54:10 +0100

    Remove Config loading methods

--- src/main/java/org/pistonmc/configuration/file/Config.java
+++ src/main/java/org/pistonmc/configuration/file/Config.java
@@ -1,5 +1,6 @@
 package org.pistonmc.configuration.file;
 
+import org.pistonmc.Piston;
 import org.pistonmc.exception.configuration.InvalidConfigurationException;
 import org.pistonmc.logging.Logger;
 import org.pistonmc.logging.Logging;
@@ -96,40 +97,58 @@ public class Config extends YamlConfiguration {
 		}
 	}
 
-	public static Config load(JavaPlugin plugin, File file, String def) {
+	public static Config loadFromPlugin(JavaPlugin plugin, File file, String def) {
         Config config = new Config(plugin, file, def);
         config.reload();
         return config;
 	}
 
-	public static Config load(JavaPlugin plugin, File file) {
-		return load(plugin, file, file.getName());
+	public static Config loadFromPlugin(JavaPlugin plugin, File file) {
+		return loadFromPlugin(plugin, file, file.getName());
 	}
 
-	public static Config load(JavaPlugin plugin, String name, String def) {
-		return load(plugin, new File(plugin.getDataFolder(), name), def);
+	public static Config loadFromPlugin(JavaPlugin plugin, String name, String def) {
+		return loadFromPlugin(plugin, new File(plugin.getDataFolder(), name), def);
 	}
 
-	public static Config load(JavaPlugin plugin, String name) {
-		return load(plugin, new File(plugin.getDataFolder(), name), name);
+	public static Config loadFromPlugin(JavaPlugin plugin, String name) {
+		return loadFromPlugin(plugin, new File(plugin.getDataFolder(), name));
 	}
 
-    public static Config load(File jar, File file, String def) {
+    public static Config loadFromFile(File jar, File file, String def) {
         Config config = new Config(jar, file, def);
         config.reload();
         return config;
     }
 
-    public static Config load(File jar, File file) {
-        return load(jar, file, file.getName());
+    public static Config loadFromFile(File jar, File file) {
+        return loadFromFile(jar, file, file.getName());
     }
 
-    public static Config load(File jar, String name, String def) {
-        return load(jar, new File(name), def);
+    public static Config loadFromFile(File jar, String name, String def) {
+        return loadFromFile(jar, new File(name), def);
     }
 
-    public static Config load(File jar, String name) {
-        return load(jar, new File(name), name);
+    public static Config loadFromFile(File jar, String name) {
+        return loadFromFile(jar, new File(name));
+    }
+
+    public static Config loadFromRoot(File file, String def) {
+        Config config = new Config(Piston.getServerJar(), file, def);
+        config.reload();
+        return config;
+    }
+
+    public static Config loadFromRoot(File file) {
+        return loadFromRoot(file, file.getName());
+    }
+
+    public static Config loadFromRoot(String name, String def) {
+        return loadFromRoot(new File(name), def);
+    }
+
+    public static Config loadFromRoot(String name) {
+        return loadFromRoot(new File(name));
     }
 
     private static InputStream getResource(File file, String name) {
