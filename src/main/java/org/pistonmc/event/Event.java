4539b0aebfc6f5e9f5e32cbe0bb046c65473ab16
Harry Fox <harry.fox.ic@icloud.com>
2014-08-28 03:33:42 +0100

    Add toString() to Event by default

--- src/main/java/org/pistonmc/event/Event.java
+++ src/main/java/org/pistonmc/event/Event.java
@@ -1,4 +1,12 @@
 package org.pistonmc.event;
 
+import org.pistonmc.util.ClassUtils;
+
 public class Event {
+
+    @Override
+    public String toString() {
+        return ClassUtils.build(this, true);
+    }
+
 }
