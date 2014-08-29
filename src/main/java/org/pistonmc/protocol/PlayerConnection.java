06731eedf7ad71b9c0846ca44f97709d6a7bf68b
Harry Fox <harry.fox.ic@icloud.com>
2014-08-29 13:51:12 +0100

    Methods to secure PlayerConnections

--- src/main/java/org/pistonmc/protocol/PlayerConnection.java
+++ src/main/java/org/pistonmc/protocol/PlayerConnection.java
@@ -3,9 +3,18 @@ package org.pistonmc.protocol;
 import org.pistonmc.exception.protocol.packet.PacketException;
 import org.pistonmc.protocol.packet.OutgoingPacket;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
 
 public interface PlayerConnection {
+
+    public void secure(SecretKey key);
+
+    public void unsecure();
+
+    public boolean isSecured();
+
+    public SecretKey getSecretKey();
 
     public void sendPacket(OutgoingPacket packet) throws PacketException, IOException;
 
