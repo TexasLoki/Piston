2d687a76fc5c25b89c4137f591b9d9c7b9638914
Harry Fox <harry.fox.ic@icloud.com>
2014-09-01 18:48:46 +0100

    Add getJoinedString to CommandArguments

--- src/main/java/org/pistonmc/commands/CommandArguments.java
+++ src/main/java/org/pistonmc/commands/CommandArguments.java
@@ -1,6 +1,10 @@
 package org.pistonmc.commands;
 
+import org.pistonmc.util.OtherUtils;
+
 public class CommandArguments {
+
+    private static String defJoin = " ";
 
     private String command;
     private String[] array;
@@ -12,6 +16,44 @@ public class CommandArguments {
 
     public int length() {
         return array.length;
+    }
+
+    public String getJoinedString(int start, int end, String join) {
+        if(start >= array.length) {
+            throw new IndexOutOfBoundsException("Start point (" + start + ") is exceeds array end point");
+        }
+
+        if(end >= array.length) {
+            throw new IndexOutOfBoundsException("End point (" + end + ") is exceeds array end point");
+        }
+
+        int i2 = 0;
+        String[] strings = new String[array.length - start];
+        for(int i = start; i < array.length; i++, i2++) {
+            strings[i2] = array[i];
+        }
+
+        return OtherUtils.join(strings, join);
+    }
+
+    public String getJoinedString(int start, int end) {
+        return getJoinedString(start, end, defJoin);
+    }
+
+    public String getJoinedString(int start, String join) {
+        return getJoinedString(start, array.length - 1, join);
+    }
+
+    public String getJoinedString(int start) {
+        return getJoinedString(start, defJoin);
+    }
+
+    public String getJoinedString(String join) {
+        return getJoinedString(0, join);
+    }
+
+    public String getJoinedString() {
+        return getJoinedString(0, defJoin);
     }
 
     public String getString(int position) {
@@ -33,7 +75,9 @@ public class CommandArguments {
     }
 
     public String[] getArguments() {
-        return array;
+        String[] copy = new String[array.length];
+        System.arraycopy(array, 0, copy, 0, array.length);
+        return copy;
     }
 
     public String getCommand() {
